fecd04abb6202f4bb16620b95b073e225b0af1e1
lucas <lman817@gmail.com>
2017-08-14 18:02:48 -0700

    bbc tests

--- bin/test.js
+++ bin/test.js
@@ -2,7 +2,10 @@
 
 let test = (() => {
   var _ref = _asyncToGenerator(function* () {
-    yield (0, _events.trigger)('scan', { "url": "http://www.cnn.com", "articleSelector": "article", "headerSelector": null, "sectionSelector": null, "sectionNameAttribute": null });
+    yield (0, _events.trigger)('scan', { "url": "http://www.bbc.com/news", "articleSelector": ".gs-c-promo-body", "headerSelector": ".gs-c-promo-heading__title", "sectionSelector": 'div[role="region"]', "sectionNameAttribute": "aria-labelledby" });
+
+    //await trigger('scan', {"url": "http://www.theguardian.com", "articleSelector": ".fc-item__content", "headerSelector": ".fc-item__header", "sectionSelector": null, "sectionNameAttribute": null })
+    //await trigger('scan', {"url": "http://www.cnn.com", "articleSelector": "article", "headerSelector": null, "sectionSelector": null, "sectionNameAttribute": null })
     //await trigger('scan', {"url": "http://www.washingtonpost.com", "articleSelector": ".headline", "headerSelector": null, "sectionSelector": ".chain-wrapper", "sectionNameAttribute": "data-chain-name" })
     //await trigger('scan', {"url": "http://www.nytimes.com", "articleSelector": "article", "headerSelector": ".story-heading", "sectionSelector": "section", "sectionNameAttribute": "id" })
   });
--- src/test.js
+++ src/test.js
@@ -5,7 +5,10 @@ process.on('unhandledRejection', r => console.log(r));
 
 
 async function test() {
-  await trigger('scan', {"url": "http://www.cnn.com", "articleSelector": "article", "headerSelector": null, "sectionSelector": null, "sectionNameAttribute": null })
+  await trigger('scan', {"url": "http://www.bbc.com/news", "articleSelector": ".gs-c-promo-body", "headerSelector": ".gs-c-promo-heading__title", "sectionSelector": 'div[role="region"]', "sectionNameAttribute": "aria-labelledby" })
+  
+  //await trigger('scan', {"url": "http://www.theguardian.com", "articleSelector": ".fc-item__content", "headerSelector": ".fc-item__header", "sectionSelector": null, "sectionNameAttribute": null })
+  //await trigger('scan', {"url": "http://www.cnn.com", "articleSelector": "article", "headerSelector": null, "sectionSelector": null, "sectionNameAttribute": null })
   //await trigger('scan', {"url": "http://www.washingtonpost.com", "articleSelector": ".headline", "headerSelector": null, "sectionSelector": ".chain-wrapper", "sectionNameAttribute": "data-chain-name" })
   //await trigger('scan', {"url": "http://www.nytimes.com", "articleSelector": "article", "headerSelector": ".story-heading", "sectionSelector": "section", "sectionNameAttribute": "id" })
 }
