bd89346f92d4be9e508f90836713ea2002ce4b7c
lucas <lman817@gmail.com>
2017-08-15 11:05:46 -0700

    better error handling

--- bin/test.js
+++ bin/test.js
@@ -2,12 +2,12 @@
 
 let test = (() => {
   var _ref = _asyncToGenerator(function* () {
-    yield (0, _events.trigger)('scan_all', {});
+    //await trigger('scan_all', {})
 
     //await trigger('scan', {"url": "http://www.theguardian.com", "articleSelector": ".fc-item__content", "headerSelector": ".fc-item__header", "sectionSelector": null, "sectionNameAttribute": null })
     //await trigger('scan', {"url": "http://www.cnn.com", "articleSelector": "article", "headerSelector": null, "sectionSelector": null, "sectionNameAttribute": null })
     //await trigger('scan', {"url": "http://www.washingtonpost.com", "articleSelector": ".headline", "headerSelector": null, "sectionSelector": ".chain-wrapper", "sectionNameAttribute": "data-chain-name" })
-    //await trigger('scan', {"url": "http://www.nytimes.com", "articleSelector": "article", "headerSelector": ".story-heading", "sectionSelector": "section", "sectionNameAttribute": "id" })
+    yield (0, _events.trigger)('scan', { "url": "http://www.nytimes.com", "articleSelector": "article", "headerSelector": ".story-heading", "sectionSelector": "section", "sectionNameAttribute": "id" });
   });
 
   return function test() {
--- src/test.js
+++ src/test.js
@@ -5,12 +5,12 @@ process.on('unhandledRejection', r => console.log(r));
 
 
 async function test() {
-  await trigger('scan_all', {})
+  //await trigger('scan_all', {})
   
   //await trigger('scan', {"url": "http://www.theguardian.com", "articleSelector": ".fc-item__content", "headerSelector": ".fc-item__header", "sectionSelector": null, "sectionNameAttribute": null })
   //await trigger('scan', {"url": "http://www.cnn.com", "articleSelector": "article", "headerSelector": null, "sectionSelector": null, "sectionNameAttribute": null })
   //await trigger('scan', {"url": "http://www.washingtonpost.com", "articleSelector": ".headline", "headerSelector": null, "sectionSelector": ".chain-wrapper", "sectionNameAttribute": "data-chain-name" })
-  //await trigger('scan', {"url": "http://www.nytimes.com", "articleSelector": "article", "headerSelector": ".story-heading", "sectionSelector": "section", "sectionNameAttribute": "id" })
+  await trigger('scan', {"url": "http://www.nytimes.com", "articleSelector": "article", "headerSelector": ".story-heading", "sectionSelector": "section", "sectionNameAttribute": "id" })
 }
 
 test()
