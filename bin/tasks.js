f45b6f75e7b3ea86b8152930c0f386d9ea80d2ed
lucas <lman817@gmail.com>
2017-08-19 18:06:07 -0700

    platform

--- bin/tasks.js
+++ bin/tasks.js
@@ -193,18 +193,41 @@ let scanPages = exports.scanPages = (() => {
               url: data.url,
               placements: articles
             });
-            /*
-            for (let a of articles.sort(function(a, b) {
-              return a.top - b.top
-            })) {
-              console.log(JSON.stringify({
-                url: a.url,
-                title: a.title,
-                top: a.top,
-                fontSize: a.fontSize,
-                section: a.section
-              }))
-            }*/
+
+            var _iteratorNormalCompletion4 = true;
+            var _didIteratorError4 = false;
+            var _iteratorError4 = undefined;
+
+            try {
+              for (var _iterator4 = articles.sort(function (a, b) {
+                return a.top - b.top;
+              })[Symbol.iterator](), _step4; !(_iteratorNormalCompletion4 = (_step4 = _iterator4.next()).done); _iteratorNormalCompletion4 = true) {
+                let a = _step4.value;
+
+                console.log(JSON.stringify({
+                  url: a.url,
+                  title: a.title,
+                  top: a.top,
+                  fontSize: a.fontSize,
+                  section: a.section,
+                  platform: a.platform
+                }));
+              }
+            } catch (err) {
+              _didIteratorError4 = true;
+              _iteratorError4 = err;
+            } finally {
+              try {
+                if (!_iteratorNormalCompletion4 && _iterator4.return) {
+                  _iterator4.return();
+                }
+              } finally {
+                if (_didIteratorError4) {
+                  throw _iteratorError4;
+                }
+              }
+            }
+
             console.log("Found " + articles.length + " articles on " + data.name);
           }), 3000);
         } catch (err) {
